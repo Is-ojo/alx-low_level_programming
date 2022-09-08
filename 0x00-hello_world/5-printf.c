4f7f5b293e74a3e8622ff3bb6042a25c2747dd7b
Is-ojo <is.ojo.ak@gmail.com>
2022-09-08 14:45:23 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'uses the printf to print to stdoutput'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+
+
+}
