ebb9e250580609377470bb52c1f829c14f092b3b
Is-ojo <is.ojo.ak@gmail.com>
2022-09-08 16:21:43 -0700

    the final trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
+ines (11 sloc)  243 Bytes
+
 #include <stdio.h>
 /**
  * main - My enter point
  * Description: This is a function to print something out
- * Return: Always 0 (sucess)
+ * Return: Always 1 (sucess)
  */
 int main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (0);
+return (1);
 }
