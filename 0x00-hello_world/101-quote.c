da0f02de37218295cb2aa1283c76e1f733f7b81f
Is-ojo <is.ojo.ak@gmail.com>
2022-09-08 15:53:08 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - My enter point
+ * Description: This is a function to print something out
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
