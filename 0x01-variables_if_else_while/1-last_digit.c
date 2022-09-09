f84dcf9bed1d28be2fe0aaab27aecd5551476cec
Is-ojo <is.ojo.ak@gmail.com>
2022-09-09 13:11:32 -0700

    all at a go

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,27 +3,28 @@
 #include <stdio.h>
 
 /**
- * main - this is a mono functioned program
- * Description: this code generates a  random number,and tells if it is a
- * + or - number using printf
- * Return: Always 0
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+        int n, l;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int ld;
-
-	ld = n % 10;
-	if (ld > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
-	else if (ld == 0)
-		printf("Last digit of %d is %d and is 0\n", n, ld);
-	else if (ld < 6)
-	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-	}
-	return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        l = n % 10;
+        if (l > 5)
+        {
+                printf("Last digit of %d is %d and is greater than 5\n", n, l);
+        }
+        else if (l == 0)
+        {
+                printf("Last digit of %d is %d and is 0\n", n, l);
+        }
+        else
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+        }
+        return (0);
 }
