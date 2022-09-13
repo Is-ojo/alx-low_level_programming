f00bc8520ab7b5c0116ad148a7724e2f59552e04
Is-ojo <is.ojo.ak@gmail.com>
2022-09-13 03:09:19 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is my fuction to print something
+ * Description: this is just a simple printf function
+ * Return: Always 0
+ */
+void main(void)
+{
+printf("_putchar\n");
+
+} 
