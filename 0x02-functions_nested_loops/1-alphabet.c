07910bed9a4473023e41ad6f36206cb9cc60f628
Is-ojo <is.ojo.ak@gmail.com>
2022-09-13 08:34:28 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * void print_alphabet(void) - prints alphabet
+ * Description: this prints the alphabet using a for loop
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+_putchar('\n');
+}
